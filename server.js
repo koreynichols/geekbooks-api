96b3b89c08baf07e7ffa36edb084e197b4885b44
koreynichols <koreynichols1@gmail.com>
2020-06-20 22:49:56 -0700

    created connection to mongoDB and tested with test user model

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.use(
     bodyParser.urlencoded({
@@ -11,6 +25,10 @@ app.use(
 );
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
+
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
+
